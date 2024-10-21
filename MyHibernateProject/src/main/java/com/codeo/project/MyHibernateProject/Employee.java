41f19292195c6465dcce25dc3f1e8235925509e7
subhoku <career@codeographsolutions.com>
2024-10-21 09:16:27 +0530

    This is Hibernate first project using mysql

--- MyHibernateProject/src/main/java/com/codeo/project/MyHibernateProject/Employee.java
+++ MyHibernateProject/src/main/java/com/codeo/project/MyHibernateProject/Employee.java
@@ -1,40 +1,24 @@
 package com.codeo.project.MyHibernateProject;
 
-public class Employee {
+import java.io.Serializable;
 
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Entity;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "employeex")
+@Data
+public class Employee implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
 	private int emp_id;
 	private String emp_name;
 	private double emp_salary;
 	private String emp_address;
-	public int getEmp_id() {
-		return emp_id;
-	}
-	public void setEmp_id(int emp_id) {
-		this.emp_id = emp_id;
-	}
-	public String getEmp_name() {
-		return emp_name;
-	}
-	public void setEmp_name(String emp_name) {
-		this.emp_name = emp_name;
-	}
-	public double getEmp_salary() {
-		return emp_salary;
-	}
-	public void setEmp_salary(double emp_salary) {
-		this.emp_salary = emp_salary;
-	}
-	public String getEmp_address() {
-		return emp_address;
-	}
-	public void setEmp_address(String emp_address) {
-		this.emp_address = emp_address;
-	}
-	@Override
-	public String toString() {
-		return "Employee [emp_id=" + emp_id + ", emp_name=" + emp_name + ", emp_salary=" + emp_salary + ", emp_address="
-				+ emp_address + "]";
-	}
-	
+
 	
 }
--- MyHibernateProject/src/main/java/com/codeo/project/MyHibernateProject/Runner.java
+++ MyHibernateProject/src/main/java/com/codeo/project/MyHibernateProject/Runner.java
@@ -1,19 +1,32 @@
 package com.codeo.project.MyHibernateProject;
 
+
+
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		//create session from HibernateUtil
 	
-		Session session=HibernateUtil.getSessionFactory().openSession();
-           Transaction transaction=null;
-           transaction=session.beginTransaction();
-           //example HQL to create a table
- String hql=""           
-           
+		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+	     Session session=sessionFactory.openSession();
+	     Transaction transaction=session.beginTransaction();
+	     
+	     Employee employee=new Employee();
+	     employee.setEmp_id(1);
+	     employee.setEmp_name("sudarshan");
+	     employee.setEmp_salary(982372.32);
+	     employee.setEmp_address("Nagpur");
+	   
+	     
+	     session.save(employee);
+	     transaction.commit();
+	     session.close();
+	     sessionFactory.close();
 	}
 
 }
